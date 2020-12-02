0b931f7fee7f6183958b55b9b65746b6c0f7f773
Rashid <rashideram2@gmail.com>
2020-12-02 15:12:56 +0600

    v1--demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./styles/App.scss";
+import Header from "./components/header"
+import Banner from "./components/banner";
 
 function App() {
+  useEffect(()=>{
+    let vh = window.innerHeight * .01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+  }, [])
   return (
     <div className='App'>
-      <h2>Hello this is your starter files </h2>
+      <Header />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/banner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {ReactComponent as RightArrow } from "../assets/arrow-right.svg"
+
+const Banner = () => {
+    return (
+        <section className="main">
+            <div className="container">
+                <div className="row">
+                    <h2>
+                        <div className="line">
+                            <span>Making Stuff Out Of</span>
+                        </div>
+                        <div className="line">
+                            <span>Sheer Curiosity</span>
+                        </div>
+                    </h2>
+                    <div className="btn-row">
+                        <a href="/">About Me <RightArrow /></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Banner;
