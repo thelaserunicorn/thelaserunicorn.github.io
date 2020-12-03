7701208df2927b03989036ef1fdbb2c0b89b73b0
Rashid <rashideram2@gmail.com>
2020-12-03 11:35:24 +0600

    added animations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, {useEffect} from "react";
+import gsap from "gsap";
 import "./styles/App.scss";
 import Header from "./components/header"
 import Banner from "./components/banner";
 import Cases from "./components/cases";
+import IntroOverlay from "./components/introOverlay"
 
 function App() {
   useEffect(()=>{
     let vh = window.innerHeight * .01;
     document.documentElement.style.setProperty("--vh", `${vh}px`)
+    //whiteflash fix
+    gsap.to('body', 0, {css: {visibility: 'visible'}})
+    
+    const tl = gsap.timeline();
+    tl.from('.line span', 1.75, {
+      y: 100,
+      ease: 'power4.out',
+      delay: 1.35,
+      skewY: 7,
+      stagger: {
+        amount: 0.35
+      }
+    }).to('.overlay-top', 1.6, {
+      height: 0,
+      ease: "expo.inOut",
+      stagger: 0.4,
+    }).to('.overlay-bottom', 1.6, {
+      width: 0,
+      ease: 'expo.inOut',
+      delay: -0.8,
+      stagger:{
+        amount: 0.4,
+      }
+
+    }).to('.intro-overlay', 0, {
+      css:{display: 'none'}
+    })
+    .from('.case-image img', 1.6, {
+      scale: 1.4,
+      ease: 'expo.inOut',
+      delay: -2,
+      stagger: {
+        amount: 0.4
+      }
+    })
+
+
   }, [])
   return (
     <div className='App'>
+      <IntroOverlay />
       <Header />
       <Banner />
       <Cases />
