7cf3a3e49b914a1a5e5279dc2da97a0bfd202499
Rashid <rashideram2@gmail.com>
2020-12-05 18:05:49 +0600

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,27 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className="page">
+        <section className="main">
             <div className="container">
                 <div className="row">
-                    <h3>About Page</h3>
+                    <h2>
+                        <div className="line">
+                            <span>Hey ! I Am Rashid</span>
+                        </div>
+                        <div className="line">
+                            <span>And I Like Building Things.</span>
+                        </div>
+                    </h2>
+                    <h3> Et haribero temcumque nihil impedit quo minus id quod maxime placeat facere possimus, 
+                        omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et 
+                        aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae 
+                        sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, 
+                        ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
+                    </h3>
+
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
